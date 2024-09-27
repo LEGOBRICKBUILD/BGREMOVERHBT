9ce31b8f83fd115350865ece51b01ad08311518b
person <124001219+LEGOBRICKBUILD@users.noreply.github.com>
2024-09-26 20:02:14 -0400

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["hi guys!", "made by justyn", "doing hw rn", "sub 2 youtube.com/c/legobrickbuild"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
