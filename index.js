db836f1d3ba122b5dbf2cac3324e94efab312279
person <124001219+LEGOBRICKBUILD@users.noreply.github.com>
2024-09-28 11:25:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Made by Justyn Lam 👑", "normal😇dreams or wet💦dreams😨", "Looking for status suggestions, dm justyn or ping him!", "balls", "hi everyone!!", "Listening to Joes Noms 🔥🔥", "normal😇dreams or wet💦dreams😨"];
+const statusMessages = ["Made by Justyn Lam 👑", "normal😇dreams or wet💦dreams😨", "do u like homework", "Looking for status suggestions, dm justyn or ping him!", "balls", "hi everyone!!", "Listening to Joes Noms 🔥🔥", "minecraft roblox or val pick one", "q23rfqwerfqwerdfd"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
